198abe358a9dcb4bba4eb5373d8e083ce74d346e
Marcos Corsi <marcosvcorsi@gmail.com>
2021-05-02 16:11:12 -0300

    feat: finish first class

--- frontend/public/app.js
+++ frontend/public/app.js
@@ -1,5 +1,8 @@
 let bytesAmount = 0;
 
+const API_URL = 'http://localhost:3000';
+const ON_UPLOAD_EVENT = 'file-uploaded';
+
 const formatBytes = (bytes, decimals = 2) => {
   if(bytes === 0) return '0 Bytes';
 
@@ -47,8 +50,53 @@ const showSize = () => {
   }, 50);
 }
 
+const updateMessage = (message) => {
+  const msg = document.getElementById('msg');
+
+  msg.innerText = message;
+
+  msg.classList.add('alert', 'alert-success');
+
+  setTimeout(() => {
+    msg.hidden = true;
+  }, 3000)
+}
+
+const showMessage = () => {
+  const urlParams = new URLSearchParams(window.location.search);
+
+  const serverMessage = urlParams.get('msg');
+
+  if(!serverMessage) return;
+
+  updateMessage(serverMessage);
+}
+
+const configureForm = (targetUrl) => {
+  const form = document.getElementById('form');
+  form.action = targetUrl;
+}
+
 const onload = () => {
-  console.log('loaded');
+  showMessage();
+
+  const ioClient = io.connect(API_URL, { withCredentials: false });
+  
+  ioClient.on('connect', (msg) => {
+    console.log('connected', ioClient.id);
+
+    const targetUrl = API_URL + `?socketId=${ioClient.id}`;
+
+    configureForm(targetUrl);
+  })
+  
+  ioClient.on(ON_UPLOAD_EVENT, (bytesReceived) => {
+    console.log(bytesReceived);
+    bytesAmount = bytesAmount - bytesReceived;
+    updateStatus(bytesAmount);
+  })
+
+  updateStatus(0);
 }
 
 window.onload = onload;
